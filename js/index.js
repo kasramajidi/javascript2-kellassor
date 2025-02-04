a19d493791bac3c65973ff55ca721aa2c2ec68dc
kasra <kasramajdy81@gmail.com>
2025-02-04 12:21:09 +0330

    Create a button that fetches a random joke from Chuck Norris API and shows a loading state while fetching.

--- js/index.js
+++ js/index.js
@@ -17,17 +17,39 @@
 
 // روش اول استفاده از reduce هست
 
-function reduceArray(inputArray) {
-    return inputArray.reduce((acc, value) => Array.isArray(value) ? acc.concat(reduceArray(value)) : acc.concat(value), [])
-}
+// function reduceArray(inputArray) {
+//     return inputArray.reduce((acc, value) => Array.isArray(value) ? acc.concat(reduceArray(value)) : acc.concat(value), [])
+// }
 
-console.log(reduceArray([1, [2, [3, [4]], 5]]))
+// console.log(reduceArray([1, [2, [3, [4]], 5]]))
 
-// داستان تابع اول اینطوری که وقتی ارایع اعداد وارد میشه  ردیوس کنه و دوتا ورودی بهش میدم ک یکی برای مفدار و یکی برای چیزی که میخوام انباشته کنیم که با شرط این کار انجام میدیم
+// // داستان تابع اول اینطوری که وقتی ارایع اعداد وارد میشه  ردیوس کنه و دوتا ورودی بهش میدم ک یکی برای مفدار و یکی برای چیزی که میخوام انباشته کنیم که با شرط این کار انجام میدیم
 
-// روش دوم از متد flat
-function flatArray(inputArray) {
-    return inputArray.flat(Infinity)
-}
+// // روش دوم از متد flat
+// function flatArray(inputArray) {
+//     return inputArray.flat(Infinity)
+// }
 
-console.log(flatArray([1, [2, [3, [4]], 5]]))
+// console.log(flatArray([1, [2, [3, [4]], 5]]))
+
+// task 3
+
+const btn = document.querySelector(".btn")
+const spanAll = document.querySelector(".div-span")
+
+btn.addEventListener("click", () => jokeFunction())
+
+const jokeFunction = async () => {
+    const url = "https://api.chucknorris.io/jokes/random"
+
+    const response = await fetch(url, {
+        method: "GET"
+    })
+
+    const data = await response.json();
+
+
+    spanAll.textContent = data.value
+
+};
+
